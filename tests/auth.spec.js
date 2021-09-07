71ac365df9ddda8d846d933cbff2fa3c4aa32d7e
Tatsiana Astrouskaya <82601614+tattyola@users.noreply.github.com>
2021-09-06 22:21:29 -0400

    add api tests (#2)

---
+++ tests/auth.spec.js
@@ -0,0 +1,30 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+
+
+
+describe('successful login', function() {
+    const request = supertest('https://paysis.herokuapp.com');
+
+    it('successful log in', function() {
+        request
+            .post('/auth')
+            .send({login:'adminius', password:'supers3cret'})
+            .expect(200)
+            .end(function(err, res) {
+                //console.log(res);
+                expect(res.statusCode).to.eq(200);
+                expect(res.body.token).not.to.be.undefined;
+            });
+    });
+
+    it('login with wrong credentials should return error', function () {
+        request
+            .post('/auth')
+            .send({login:'wrong', password:'wrong'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(404);
+                expect(res.body.message).to.eq('Wrong login or password.');
+            });
+    });
+})
