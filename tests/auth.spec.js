bf8a40bbb0d82f29a5f7cfb1ffdd29dee03516c6
Tatsiana A <astrouskaya@yahoo.com>
2021-09-12 21:46:59 -0400

    wrappers

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,44 +1,34 @@
-import supertest from 'supertest';
 import { expect } from 'chai';
+import AuthHelper from '../helpers/auth.helper';
 
 describe('auth', function() {
-    let result;
+    let authHelper = new AuthHelper();
 
     describe('successful login', function () {
         before(async function () {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login:process.env.LOGIN, password:process.env.PASSWORD})
-                .then(res => {
-                    result = res;
-                });
+            await authHelper.get(process.env.LOGIN, process.env.PASSWORD);
         });
 
         it('response status code is 200', function () {
-            expect(result.statusCode).to.eq(200);
+            expect(authHelper.response.statusCode).to.eq(200);
         });
         
         it('response body contains authorization token', function () {
-            expect(result.body.token).not.to.be.undefined;
+            expect(authHelper.response.body.token).not.to.be.undefined;
         });
     });
     
     describe('login with wrong credentials should return error', function () {
         before(async function () {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login:'wrong', password:'wrong'})
-                .then(res => {
-                    result = res;
-                });
+            await authHelper.get('invalid', 'invalid');
         });
 
         it('response status code is 404', function () {
-            expect(result.statusCode).to.eq(404);
+            expect(authHelper.response.statusCode).to.eq(404);
         });
 
         it('response body contains error message', function () {
-            expect(result.body.message).to.eq('Wrong login or password.');
+            expect(authHelper.response.body.message).to.eq('Wrong login or password.');
         });
     });
 });
