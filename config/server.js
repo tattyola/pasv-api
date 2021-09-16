06b48c911315d7160edfd7becedd13ede006f940
Tatsiana Astrouskaya <82601614+tattyola@users.noreply.github.com>
2021-09-16 17:26:41 -0400

    add users tests (#5)

--- config/server.js
+++ config/server.js
@@ -1,2 +1,10 @@
 import { config } from 'dotenv';
+import AuthHelper from '../helpers/auth.helper';
+
 config();
+
+before (async function () {
+    let authHelper = new AuthHelper();
+    await authHelper.get(process.env.LOGIN, process.env.PASSWORD);
+    process.env.TOKEN = authHelper.response.body.token;
+});
