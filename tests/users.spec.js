e587df8e84a1bd1d37ec10acd580f5cc97f413cf
Tatsiana A <astrouskaya@yahoo.com>
2021-09-16 17:25:39 -0400

    add users tests

---
+++ tests/users.spec.js
@@ -0,0 +1,94 @@
+import { expect } from 'chai';
+import UsersHelper from '../helpers/users.helper';
+import ConfigHelper from '../helpers/config.helper';
+import { getRandomItem  } from '../helpers/common.helper';
+
+
+describe('users', function () {
+    let usersHelper = new UsersHelper();
+    let config = new ConfigHelper();
+
+    describe('user creation', function () {
+        before(async function () {
+            await usersHelper.create();
+        });
+
+        it('response statusCode is 200', function () {
+            expect(usersHelper.response.statusCode).to.eq(200);
+        });
+
+        it('response body contains user id', function () {
+            expect(usersHelper.response.body.id).not.to.be.undefined;
+        });
+
+        it('response body contains amount', function () {
+            expect(usersHelper.response.body.amount).not.to.be.undefined;
+        });
+    });
+    
+    describe('user deletion', async function () {
+        before(async function () {
+            await usersHelper.create();
+            await usersHelper.delete(usersHelper.response.body.id)
+        });
+
+        it('response statusCode is 200', function () {
+            expect(usersHelper.response.statusCode).to.eq(200);
+        });
+
+        it('response body contains message', function () {
+            expect(usersHelper.response.body.message).to.eq('User deleted.');
+        });
+    });
+
+    describe('get all', async function () {
+        before(async function () {
+            for await (const user of Array(3)) {
+                await usersHelper.create();
+            }
+            await usersHelper.getAll();
+        });
+
+        it('response statusCode is 200', function () {
+            expect(usersHelper.response.statusCode).to.eq(200);
+        });
+
+        it('response body contains list of 3 or more items', function () {
+            expect(usersHelper.response.body.length).to.be.at.least(3);
+        });
+
+        it('response body list item contains id', function () {
+            expect(getRandomItem(usersHelper.response.body).id).not.to.be.undefined;
+        });
+
+        it('response body list item contains amount ', function () {
+            expect(getRandomItem(usersHelper.response.body).amount).not.to.be.undefined;
+        });
+    });
+
+    describe('get specific user', async function () {
+        before(async function () {
+            for await (const user of Array(3 )) {
+                await usersHelper.create();
+                await usersHelper.getSpecific(usersHelper.response.body.id);
+            }
+        });
+
+        it('response statusCode is 200', function () {
+            expect(usersHelper.response.statusCode).to.eq(200);
+        });
+
+        it('response body contains user id', function () {
+            expect(usersHelper.response.body.id).not.to.be.undefined;
+        });
+
+        it('response body contains amount', function () {
+            expect(usersHelper.response.body.amount).not.to.be.undefined;
+        });
+    });
+
+    after(async function () {
+        await config.wipeData();
+    })
+});
+
